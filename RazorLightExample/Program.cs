1d0c8ec8af3950a84a7a5e205effe7d0ede2e6bf
許呈善 Albert Hsu <pt019@cathaysite.com.tw>
2023-02-17 15:14:38 +0800

    no message

--- RazorLightExample/Program.cs
+++ RazorLightExample/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RazorLight;
+using System.Reflection;
+
+// ...
+
+var engine = new RazorLightEngineBuilder()
+    .UseEmbeddedResourcesProject(Assembly.GetExecutingAssembly())
+    .UseMemoryCachingProvider()
+    .Build();
+
+//var templateKey = "Namespace.Path.To.Template.cshtml";
+var templateKey = "RazorLightExample.EmailTemplates.Body.cshtml";
+var model = new
+{
+    Title = "Hello, RazorLight!",
+    Items = new List<string> {
+        "Item 1",
+        "Item 2",
+        "Item 3"
+    }
+};
+
+string result = await engine.CompileRenderAsync(templateKey, model);
+
+Console.WriteLine(result);
